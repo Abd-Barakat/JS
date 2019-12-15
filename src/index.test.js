e9ccb94b937039e6044527f9f9d11cce75769f66
abd barakat <abdbarakat050@gmail.com>
2019-12-15 10:11:13 +0200

    test should fail

--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('index.html', () => {
         const file = fs.readFileSync('./src/index.html', 'utf-8');
         jsdom.env(file, function(err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal('Hello Abdel-Rahman Saleh Barakat !');
+            expect(h1.innerHTML).to.equal('Hello Abdel-Rahman Saleh Barakat ??');
             // this to convert env method from asynchronous to synchronous
             finish();
             window.close();
